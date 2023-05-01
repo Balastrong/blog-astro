b95cb5472d84d9bcb2bc0c3d6ac1e8a6e4b20b66
Leonardo Montini <lion.48m@gmail.com>
2023-05-01 23:26:12 +0200

    Resolve images

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -3,7 +3,7 @@ import { Post } from '~/types';
 const load = async function () {
   let images: Record<string, () => Promise<unknown>> | undefined = undefined;
   try {
-    images = import.meta.glob('~/assets/images/**');
+    images = import.meta.glob('~/content/post/**');
   } catch (e) {
     // continue regardless of error
   }
@@ -24,23 +24,23 @@ export const findImage = async ({ image: imagePath, id: postId }: Pick<Post, 'im
     return null;
   }
 
-  if (imagePath.startsWith('./') && !!postId) {
-    const postPath = postId.split('/').slice(0, -1).join('/');
-    const image = imagePath.replace('./', '');
-
-    return `/src/content/post/${postPath}/${image}`;
-  }
-
   if (imagePath.startsWith('http://') || imagePath.startsWith('https://') || imagePath.startsWith('/')) {
     return imagePath;
   }
 
-  if (!imagePath.startsWith('~/assets')) {
+  if (!imagePath.startsWith('~/assets') && !imagePath.startsWith('./')) {
     return null;
   } // For now only consume images using ~/assets alias (or absolute)
 
   const images = await fetchLocalImages();
-  const key = imagePath.replace('~/', '/src/');
+  let key = imagePath.replace('~/', '/src/');
+
+  if (imagePath.startsWith('./') && !!postId) {
+    const postPath = postId.split('/').slice(0, -1).join('/');
+    const image = imagePath.replace('./', '');
+
+    key = `/src/content/post/${postPath}/${image}`;
+  }
 
   return typeof images[key] === 'function' ? (await images[key]())['default'] : null;
 };
