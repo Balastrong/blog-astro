6483bd66c9e83473f28687d926b12efa46e886e4
Leonardo Montini <lion.48m@gmail.com>
2025-07-16 10:18:34 +0200

    skip bots

--- netlify/functions/visitor-tracker.js
+++ netlify/functions/visitor-tracker.js
@@ -8,6 +8,17 @@ const redis = new Redis({
 
 exports.handler = async (event) => {
   try {
+    // Check if request is from a bot
+    const userAgent = event.headers['user-agent'] || '';
+    const isUserBot = isBot(userAgent);
+
+    if (isUserBot) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true, count: 0 }),
+      };
+    }
+
     // Get user's IP (very basic, works behind Netlify)
     const ip = event.headers['x-forwarded-for']?.split(',')[0] || event.headers['client-ip'] || 'unknown';
 
@@ -38,3 +49,37 @@ exports.handler = async (event) => {
     };
   }
 };
+
+// Function to check if the request is from a bot
+function isBot(userAgent) {
+  if (!userAgent) return true;
+
+  const botPatterns = [
+    /bot/i,
+    /crawler/i,
+    /spider/i,
+    /googlebot/i,
+    /bingbot/i,
+    /yahoo/i,
+    /yandex/i,
+    /baidu/i,
+    /slurp/i,
+    /facebook/i,
+    /crawl/i,
+    /scrape/i,
+    /archive\.org/i,
+    /ahrefs/i,
+    /semrush/i,
+    /rogerbot/i,
+    /mj12bot/i,
+    /lighthouse/i,
+    /pingdom/i,
+    /headless/i,
+    /chrome-lighthouse/i,
+    /dataminr/i,
+    /screaming frog/i,
+    /sitebulb/i,
+  ];
+
+  return botPatterns.some((pattern) => pattern.test(userAgent));
+}
