51794e260fcf010a5a6f9f6c98e6610280b4f5c9
Leonardo Montini <lion.48m@gmail.com>
2025-11-25 22:40:25 +0100

    fix rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,7 +4,7 @@ import { SITE, BLOG } from '~/config.mjs';
 import { fetchPosts } from '~/utils/blog';
 import { getPermalink } from '~/utils/permalinks';
 
-export const get = async () => {
+export const GET = async () => {
   if (BLOG.disabled) {
     return new Response(null, {
       status: 404,
@@ -26,6 +26,6 @@ export const get = async () => {
       pubDate: post.publishDate,
     })),
 
-    trailingSlash: SITE.trailingSlash
+    trailingSlash: SITE.trailingSlash,
   });
 };
